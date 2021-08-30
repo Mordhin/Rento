e84eaf0882f005ffeb8ce475827de14316a265dd
Julien Corion <astram59@gmail.com>
2021-08-30 19:01:57 +0200

    navbar n first done + responsive

---
+++ src/components/First.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import call_img from '../assets/call_image.svg'
+
+const First = () => {
+  return (
+    <div className="premier">
+      <div className="call">
+        <h2>Conduisez autrement !</h2>
+        <br/>
+        <p>Dès aujourd'hui, revendez votre ancienne voiture et passez chez Rento.</p>
+        <p>Faites-vous plaisir parmi un large choix de véhicule, et changez le quand vous voulez.</p>
+        <p>C'est simple, Rento se charge de tout !</p>
+        <br/>
+        <button className="primary_btn">J'essaie !</button>
+      </div>
+      <img src={call_img} />
+    </div>
+  )
+}
+
+export default First
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,61 @@
-import React from 'react'
-import NavBlob from './NavBlob'
+import React, { useState } from 'react'
+import logo from '../assets/logo.png'
+import {GoThreeBars} from 'react-icons/go';
+import { useWindowDimensions } from '../hooks/useWindowDimensions';
 
 const Navbar = () => {
+  const {height, width} = useWindowDimensions();
+  const [scroll, setScroll] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+
+  const checkScroll = () => {
+    if (!scroll && window.pageYOffset > 1) {
+      setScroll(true);
+    } else if (scroll && window.pageYOffset <= 1) {
+      setScroll(false);
+    }
+  };
+
+  window.addEventListener("scroll", checkScroll);
+
+  const toggleDisabled = () => {
+    setDisabled(!disabled);
+  }
+
   return (
-    <div className="navbar">
-      <ul>
-        <li className="link">Accueil</li>
-        <li className="link">Qui sommes-nous ?</li>
-        <li className="link">Contact</li>
-      </ul>
-      <ul>
-        <li className="link">Se connecter</li>
-        <li className="signup">S'inscrire</li>
-      </ul>
-    </div>
+    <nav className={`navbar ${scroll ? 'scroll' : ''}`}>
+      <div className="title">
+        <img src={logo} alt="rento logo"></img>
+        <h1>Rento</h1>
+      </div>
+      {width >= 1000 && (
+        <div className="menu">
+          <ul className="">
+            <li className="link">Accueil</li>
+            <li className="link">Qui sommes-nous ?</li>
+            <li className="link">Contact</li>
+          </ul>
+          <ul>
+            <li className="link">Se connecter</li>
+            <li className="primary_btn">S'inscrire</li>
+          </ul>
+        </div>
+      )}
+      {width < 1000 && (
+        <div className="mini_menu">
+          <div className="bars" onClick={() => toggleDisabled()}><GoThreeBars /></div>
+          <ul className={`dropdown ${disabled ? "disabled" : ""}`}>
+            <li>Accueil</li>
+            <li>Qui sommes-nous ?</li>
+            <li>Contact</li>
+            <li>Se connecter</li>
+            <li className="secondary_btn">S'inscrire</li>
+          </ul>
+        </div>
+        
+      )}
+      
+    </nav>
   )
 }
 
