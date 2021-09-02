7b0db699d57f3cfd27cce530f9fdc52a5c2caede
Julien Corion <astram59@gmail.com>
2021-09-03 01:03:42 +0200

    starting pricing and overflow:hidden on primary_btn

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CallToAction from "./components/CallToAction";
 import HowItWorks from "./components/HowItWorks";
 import Arguments from "./components/Arguments";
 import { CarouselData } from "./components/carousel/carouselData";
+import Pricing from "./components/Pricing";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <div className="spacer layer1"></div>
       <Arguments carousel={CarouselData} />
       <HowItWorks />
+      <Pricing />
       <div className="spacer layer2"></div>
     </div>
   );
---
+++ src/components/Pricing.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <div className="pricing">
+      <div className="cards">
+        <div className="pricing-card">
+          <div className="header"></div>
+        </div>
+        <div className="pricing-card"></div>
+        <div className="pricing-card"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
