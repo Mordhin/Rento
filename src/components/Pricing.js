3a759d98ffa2e9509c50125c2e323bdeba5b0933
Julien Corion <astram59@gmail.com>
2021-09-03 02:01:07 +0200

    first draft pricing cards

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,14 +1,64 @@
 import React from "react";
+import bronze from "../assets/bronze-medal.png";
 
 const Pricing = () => {
   return (
     <div className="pricing">
       <div className="cards">
         <div className="pricing-card">
-          <div className="header"></div>
+          <div className="header silver">
+            <div className="badge">
+              <img src={bronze} />
+            </div>
+          </div>
+          <h3>Forfait Occasionnel</h3>
+          <div className="price">
+            <div>300€</div>
+            <div>par mois</div>
+          </div>
+          <ul>
+            <li>Accès à tout type de véhicule</li>
+            <li>Accès aux catégories A, B et C</li>
+            <li>Priorité haute sur l'échange</li>
+          </ul>
+          <button className="bronze">Je m'inscris</button>
         </div>
-        <div className="pricing-card"></div>
-        <div className="pricing-card"></div>
+        <div className="pricing-card">
+          <div className="header gold">
+            <div className="badge">
+              <img src={bronze} />
+            </div>
+          </div>
+          <h3>Forfait Occasionnel</h3>
+          <div className="price">
+            <div>600€</div>
+            <div>par mois</div>
+          </div>
+          <ul>
+            <li>Accès à tout type de véhicule</li>
+            <li>Accès aux catégories A, B, C et D</li>
+            <li>Priorité absolue sur l'échange</li>
+          </ul>
+          <button className="bronze">Je m'inscris</button>
+        </div>
+        <div className="pricing-card">
+          <div className="header bronze">
+            <div className="badge">
+              <img src={bronze} />
+            </div>
+          </div>
+          <h3>Forfait Occasionnel</h3>
+          <div className="price">
+            <div>150€</div>
+            <div>par mois</div>
+          </div>
+          <ul>
+            <li>Accès à tout type de véhicule</li>
+            <li>Accès aux catégories A et B</li>
+            <li>Priorité normale sur l'échange</li>
+          </ul>
+          <button className="bronze">Je m'inscris</button>
+        </div>
       </div>
     </div>
   );
